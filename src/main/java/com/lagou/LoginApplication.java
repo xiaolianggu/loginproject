752e1adb4b7b2b6b7694aa171a171601b44a1edc
gu <gu@sina.cn>
2020-03-12 00:32:06 +0800

    commit

--- src/main/java/com/lagou/LoginApplication.java
+++ src/main/java/com/lagou/LoginApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
-//import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableCaching
-//@EnableRedisHttpSession
+@EnableRedisHttpSession
 public class LoginApplication  extends SpringBootServletInitializer {
 
     @Override
